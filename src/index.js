a1ef0742b0d487186a854e3e4f1ac4781f8cecd6
Neemias Almeida <neemias.junior@gmail.com>
2022-08-02 00:49:38 +0200

    Add analytics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ReactGA from "react-ga4";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize(process.env.REACT_APP_GA_MEASUREMENT_ID);
+ReactGA.send("pageview");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
